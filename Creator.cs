47efe0e858c003eb915e13467159204ae0d2e59f
tomsol432 <39550133+tomsol432@users.noreply.github.com>
2018-11-15 08:35:44 +0100

    Initial commit

---
+++ Creator.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+namespace Stripper
+{
+    class Creator
+    {
+        readonly int sizeX = 1000;
+        readonly int sizeY = 1000;
+        Bitmap bitmap;
+
+        
+
+        public Creator()
+        {
+            bitmap = new Bitmap(sizeX, sizeY);
+        }
+
+        public Creator(int sizeX, int sizeY)
+        {
+            this.sizeX = sizeX;
+            this.sizeY = sizeY;
+            this.bitmap = new Bitmap(sizeX, sizeY);
+        }
+
+        public Bitmap PushTextData(string data)
+        {
+            
+            byte[] bytes = Encoding.ASCII.GetBytes(data);
+            
+            if (bitmap != null && bytes.Length > 0)
+            {
+                int counter = 1;
+                for (int i = 0; i < bitmap.Width; i++)
+                {
+                    for (int j = 0; j < bitmap.Height; j++)
+                    {
+                        if (counter < bytes.Length )
+                        {
+                            string yourByteString = Convert.ToString(bytes[counter], 2).PadLeft(8, '0');
+                            if (yourByteString.Length != 0)
+                            {
+                                for (int k = 0; k < yourByteString.Length; k++)
+                                {
+
+                                    if (yourByteString[k] == '0')
+                                    {
+                                        bitmap.SetPixel(i, j, Color.Red);
+                                    }
+                                    if (yourByteString[k] == '1')
+                                    {
+                                        bitmap.SetPixel(i, j, Color.Blue);
+                                    }
+                                    counter++;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
+                        
+                        
+                    }
+
+                }
+            }
+            return bitmap;
+            
+
+        }
+
+        static string ToBinaryString(Encoding encoding, string text)
+        {
+            return string.Join("", encoding.GetBytes(text).Select(n => Convert.ToString(n, 2).PadLeft(8, '0')));
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Stripper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.ShowDialog();
+            var texxxt = 
+            File.OpenText(openFile.FileName);
+            string data  = texxxt.ReadToEnd();
+            Creator c = new Creator(1000,1000);
+            Thread thread = new Thread(() => {
+            ImageCode.Dispatcher.Invoke(new Action(() => {
+                ImageCode.Source = ImageSourceForBitmap(c.pushTextData(data));
+                data = null;
+                c = null;
+            }));
+            });
+            thread.Start();
+            
+
+        }
+        public ImageSource ImageSourceForBitmap(Bitmap bmp)
+        {
+            var handle = bmp.GetHbitmap();
+            
+                return Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            
+        }
+    }
+}
